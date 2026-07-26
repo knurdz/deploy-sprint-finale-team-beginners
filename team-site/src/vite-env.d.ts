c1e525c282f1b64dd951e509151e421e2dd366b5
Sayuru Pilapitiya <sayurupilapitiya@gmail.com>
2026-07-26 11:17:30 +0530

    Revert "Merge pull request #3 from knurdz/task/T05-secret-and-config-separation"
    This reverts commit 9100ead0aef1fd68b158ef78d268dde751ce28d5, reversing
    changes made to bc8f9d82ef126ecffcdde7016e96532f0fb254ce.

--- team-site/src/vite-env.d.ts
+++ team-site/src/vite-env.d.ts
@@ -1,10 +1,1 @@
 /// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_PUBLIC_DEPLOY_LABEL?: string;
-  readonly VITE_PUBLIC_URL?: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
